199cc7e998c95719cab8df6bad2b6b1172fa1917
Allie Sterling <sterlina89@gmail.com>
2014-11-05 14:31:15 -0800

    adds ingredients resources to routes.rb

--- allie/Ada-Cooks-1/config/routes.rb
+++ allie/Ada-Cooks-1/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
 
   resources :recipes
+  resources :ingredients
   root to: "recipes#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
