1e2908190379efcb45373b566d6950ea4a9859d7
Allie Sterling <sterlina89@gmail.com>
2014-11-05 15:37:25 -0800

    started on creating form for new ingredients

--- allie/Ada-Cooks-1/app/controllers/ingredients_controller.rb
+++ allie/Ada-Cooks-1/app/controllers/ingredients_controller.rb
@@ -1,2 +1,14 @@
 class IngredientsController < ApplicationController
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(params.require(:ingredient).permit(:name))
+    if @ingredient.save
+      render "recipes/new"
+    end
+  end
+
 end
