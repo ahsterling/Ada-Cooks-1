4702d0587849b9e2b2ef6b2eaa3c0e26a81e122d
Allie Sterling <sterlina89@gmail.com>
2014-11-07 23:49:05 -0800

    can edit and delete ingredients from ingredients show page

--- allie/Ada-Cooks-1/app/controllers/ingredients_controller.rb
+++ allie/Ada-Cooks-1/app/controllers/ingredients_controller.rb
@@ -15,4 +15,22 @@ class IngredientsController < ApplicationController
     @ingredients = Ingredient.all
   end
 
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(params.require(:ingredient).permit(:name))
+      redirect_to ingredients_path
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.destroy
+      redirect_to ingredients_path
+    end
+  end
+
 end
